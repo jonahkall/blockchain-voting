f810644e950e2de722f18f74e4cf8b0b8e3ad5e8
Jonah Kallenbach <kallenbach@college.harvard.edu>
2016-05-07 00:54:25 -0400

    pthread peer stuff

---
+++ peer.cpp
@@ -0,0 +1,19 @@
+#include "peer.h"
+
+void* comm_thread (void* arg) {
+	return NULL;
+}
+
+void* processing_thread(void* arg) {
+	return NULL;
+}
+
+int main () {
+	pthread_t comm_t;
+	pthread_t processing_t;
+
+	pthread_create(&comm_t, NULL, comm_thread, NULL);
+	pthread_create(&processing_t, NULL, processing_thread, NULL);
+
+	return 0;
+}
