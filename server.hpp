963522d6b783da7ece431c2a7427ba2dc5d1e08e
Willy Xiao <willyxiao@gmail.com>
2016-05-08 22:36:23 -0400

    server override

--- server.hpp
+++ server.hpp
@@ -31,12 +31,12 @@ using onevote::Miner;
 class MinerServiceImpl final : public Miner::Service {
   public:
     MinerServiceImpl(comm_thread_args* ctap, Client* client);
-  	Status BroadcastBlock(ServerContext* context, const BlockMsg* block_msg, Empty* empty);
-  	Status BroadcastTransaction(ServerContext* context, const TransactionMsg* transaction_msg, Empty* empty);
-  	Status GetAddr(ServerContext* context, const AddrRequest* addr_req, AddrResponse* addr_resp);
-  	Status GetTransaction(ServerContext* context, const TransactionRequest* trans_req, TransactionMsg* transaction_msg);
-  	Status GetBlock(ServerContext* context, const BlockRequest* block_req, BlockMsg* block_msg);
-  	Status GetHeartbeat(ServerContext* context, const Empty* empty, Empty* dummy);
+  	Status BroadcastBlock(ServerContext* context, const BlockMsg* block_msg, Empty* empty) override;
+  	Status BroadcastTransaction(ServerContext* context, const TransactionMsg* transaction_msg, Empty* empty) override;
+  	Status GetAddr(ServerContext* context, const AddrRequest* addr_req, AddrResponse* addr_resp) override;
+  	Status GetTransaction(ServerContext* context, const TransactionRequest* trans_req, TransactionMsg* transaction_msg) override;
+  	Status GetBlock(ServerContext* context, const BlockRequest* block_req, BlockMsg* block_msg) override;
+  	Status GetHeartbeat(ServerContext* context, const Empty* empty, Empty* dummy) override;
 
   private:
     comm_thread_args* ctap_;
