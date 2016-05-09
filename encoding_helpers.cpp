e92f3fe20e3dcf67b5632b943443bde4ec19f236
Willy Xiao <willyxiao@gmail.com>
2016-05-08 21:48:20 -0400

    new include

--- encoding_helpers.cpp
+++ encoding_helpers.cpp
@@ -1,3 +1,21 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <cstdlib>
+#include <queue>
+
+#include <grpc++/grpc++.h>
+
+#include "node.grpc.pb.h"
+
+using onevote::Empty;
+using onevote::BlockMsg;
+using onevote::TransactionMsg;
+using onevote::AddrRequest;
+using onevote::AddrResponse;
+using onevote::TransactionRequest;
+using onevote::BlockRequest;
+
 transaction* decode_transaction(const TransactionMsg* transaction_msg) {
   transaction* decoded_transaction = new transaction;
 
