a75f6084145a5f206e053b9ba66a50778c5de8e6
Ankit Gupta <ankitgupta@college.harvard.edu>
2016-05-09 15:40:52 -0400

    updated includes

--- voting-booth.hpp
+++ voting-booth.hpp
@@ -1,10 +1,7 @@
 #ifndef __VOTING_BOOTH_H__
 #define __VOTING_BOOTH_H__
 #include <openssl/sha.h>
-#include "processor.hpp"
-#include "rsa.hpp"
 #include "client.hpp"
-#include "server.hpp"
 #include <iostream>
 #include <fstream>
 #include <memory>
