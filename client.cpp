87d609aba8923fa22c338c8604d9e49bfa6ac296
Willy Xiao <willyxiao@gmail.com>
2016-05-09 23:10:42 -0400

    fix empty call

--- client.cpp
+++ client.cpp
@@ -22,14 +22,14 @@ void Client::BroadcastTransaction(transaction* transaction) {
 
 block* Client::getBlock(char* block_hash) {
   for (const auto& peer_client: peer_clients_) {
-    clientLog("Asking for block " + std::to_string(block_num) + " from " + *peer_client->peerAddr());
+    clientLog("Asking for block " + block_hash + " from " + *peer_client->peerAddr());
     block* block = peer_client->GetBlock(block_hash);
     if (block) {
       clientLog("Found!");
       return block;
     }
   }
-  clientLog("Failed to find block " + std::to_string(block_num));
+  clientLog("Failed to find block " + block_hash);
   return NULL;
 }
 
