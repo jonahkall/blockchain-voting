d397174ca1c94293e1a73020788b92ca837489a7
EC2 Default User <ec2-user@ip-172-31-24-131.us-west-2.compute.internal>
2016-05-10 03:56:01 +0000

    Fixed sudo

--- init.sh
+++ init.sh
@@ -1,13 +1,13 @@
 cd ..
-yum install build-essential autoconf libtool gcc-c++ openssl-devel
+sudo yum install build-essential autoconf libtool gcc-c++ openssl-devel
 git clone https://github.com/grpc/grpc.git
 cd grpc
 git submodule update --init
 make
 cd third_party/protobuf/
-make install
+sudo make install
 cd ../../
-make install
+sudo make install
 cd ../blockchain-voting
 export PKG_CONFIG_PATH=/home/ec2-user/grpc/libs/opt/pkgconfig/
 export LD_LIBRARY_PATH=/usr/local/lib
